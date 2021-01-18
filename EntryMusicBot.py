de8fc9a12eb92b9223442458a1a8c5bd16c7cc4e
Thomas Hancock <Thomas.Hancock@Physics.org>
2021-01-18 18:21:21 +0000

    Added json config

--- EntryMusicBot.py
+++ EntryMusicBot.py
@@ -1,15 +1,37 @@
 # bot.py
 import os
-import logging
+import sys
+import json
+import random
 
 import discord
 
-from Settings import DISCORD_TOKEN, DISCORD_GUILD, TRACK_MAP
-TOKEN = DISCORD_TOKEN
-GUILD = DISCORD_GUILD
+settings_file = sys.argv[1]
+print(f"Using settings file: {settings_file}")
 
+def proc_settings(file):
+    with open(file, "r") as read_file:
+        return json.load(read_file)
+
+data = proc_settings(settings_file)
+
+TOKEN = data["token"]
+GUILD = data["guild"]
+TRACK_MAP = data["tracks"]
+RANDOM = data["random"]
 
 class MyClient(discord.Client):
+
+    def load_tracks(self, settings_file):
+        print("Loading Track List:")
+        data = proc_settings(settings_file)
+        self.track_map = data["tracks"]
+        for user, track in self.track_map.items():
+            print(f"\t{user}:\t{track}")
+        self.random_tracks = data["random"]
+        print("\n\tRandom Tracks:")
+        for track in self.random_tracks:
+            print(f"\t\t{track}")
 
     async def on_ready(self):
         '''
@@ -47,8 +69,9 @@ class MyClient(discord.Client):
             await message.channel.send('Hello!')
 
         # Send gif
-        if message.content.startswith('!iron'):
-            await message.channel.send(file=discord.File('Ironing.gif'))
+        if message.content.startswith('!reload'):
+            self.load_tracks(settings_file)
+            await message.channel.send('Reloaded track data')
 
 
     async def on_voice_state_update(self, member, before, after):
@@ -59,27 +82,27 @@ class MyClient(discord.Client):
             return
 
         if before.channel is None and after.channel is not None:
+            print(f"Detected {member.name} joining {after.channel.name}")
             # if self.general_text_channel != None:
                 # await self.general_text_channel.send(f"{member} joined voice chat!")
 
             if self.vc != None:
+                if member.name in self.track_map:
+                    track = self.track_map[member.name]
+                else:
+                    track_no = random.randrange(0, len(self.random_tracks))
+                    if track_no > len(self.random_tracks):
+                        print("Error: Track index out of range. Default to 0.")
+                        track_no = 0
+                    track = self.random_tracks[track_no]
+
                 try:
-                    if member.name in TRACK_MAP:
-                        self.vc.play(discord.FFmpegPCMAudio(TRACK_MAP[member.name]))
-                    else:
-                        self.vc.play(discord.FFmpegPCMAudio('tracks/Zelda_Chest_Open.mp3'))
-                        if self.general_text_channel != None:
-                            await self.general_text_channel.send(f"{self.name} noticed {member} doesn't have personalised entry music. Suggest something!")
+                    self.vc.play(discord.FFmpegPCMAudio(f"tracks/{track}"))
                     self.vc.source = discord.PCMVolumeTransformer(self.vc.source, volume=0.3)
                 except discord.errors.ClientException:
                     print("User joined but already playing audio.")
 
 
 client = MyClient()
+client.load_tracks(settings_file)
 client.run(TOKEN)
-
-# @atexit.register
-# def goodbye():
-#     print("Script quit. Disconnecting client...", end="")
-#     client.disconnect()
-#     print("Done")
